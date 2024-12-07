dad102d24b44b1822b23b260d077d958bbdd361a
ghgemmer <ghgemmer@gmail.com>
2024-12-07 16:00:36 -0600

    Added retry of model list request if failed to get expected json models response

--- src/yotagrabber/models.py
+++ src/yotagrabber/models.py
@@ -9,6 +9,7 @@ from yotagrabber import config, wafbypass
 # Set to True to use local data and skip requests to the Toyota website.
 USE_LOCAL_DATA_ONLY = False
 
+DEBUG_ENABLED = False
 
 def get_models_query():
     """Read models query from a file."""
@@ -33,21 +34,49 @@ def read_local_data():
 def query_toyota():
     """Query Toyota for a list of models."""
     query = get_models_query()
-
+    
     # Get headers by bypassing the WAF.
+    print("Bypassing WAF")
     headers = wafbypass.WAFBypass().run()
-
-    # Make request.
-    json_post = {"query": query}
-    url = "https://api.search-inventory.toyota.com/graphql"
-    resp = requests.post(
-        url,
-        json=json_post,
-        headers=headers,
-        timeout=15,
-    )
-
-    return resp.json()["data"]["models"]
+    print("Getting list of models")
+    tryCount = 4
+    result = None
+    # TODO: still getting many query failures even with this retry method (goes through all retires withuot success)
+    # and not sure why?  Printed resp.text does not seem to contain any readable ascii text.
+    while tryCount:
+        # Make request.
+        json_post = {"query": query}
+        url = "https://api.search-inventory.toyota.com/graphql"
+        resp = requests.post(
+            url,
+            json=json_post,
+            headers=headers,
+            timeout=60,
+        )
+        if DEBUG_ENABLED:
+            if resp is None:
+                print("query_toyota: models query resp is None")
+            else:
+                print("query_toyota: models query request headers: ", repr(resp.request.headers))
+                print("query_toyota: models query request.body: " + str(resp.request.body))
+                print("query_toyota: models query resp", repr (resp.headers), repr(resp))
+        try:
+            result = resp.json()["data"]["models"]
+            break
+        except (requests.exceptions.JSONDecodeError) as inst:
+            print ("query_toyota: Exception occurred with accessing json models list response:", str(type(inst)) + " "  + str(inst))
+            print("resp.status_code", resp.status_code)
+            print("resp.headers", resp.headers)
+            #print("resp.text", resp.text)
+            #print("resp.content", resp.content)
+            #return None
+        tryCount -= 1
+        print("query_toyota: Trying models list query again" ,  ", tryCount = " + str(tryCount))
+        tm = 7 + (6 * random.random())
+        print("sleeping", tm, " secs")
+        sleep(tm)
+        
+    return result
 
 
 def update_models():
